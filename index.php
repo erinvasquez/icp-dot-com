1d76cb47260eb0d9dcb0566bbb16b1e7dd2076a4
erinvasquez <erin.vasquez@gmail.com>
2021-10-13 04:55:21 +0000

    Added index.php and some test code that doesn't work

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
-<? php
+<?php
 
-$dbhost = 'invisiblechairproject.com';
+// I'm guessing these are variables
+$dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
+$dbname = 'button_database';
 
-$conn = mysql_connect(($dbhost, $dbuser, $dbpass);
-mysql_select_db($dbname, $conn);
+// comments
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+mysqli_select_db($dbname, $conn);
 
-$result = mysql_query("select * from users", $conn);
+$result = mysqli_query("select * from users", $conn);
+//while ($row = mysqli_fetch(assoc($result)) {
+//   print_r($row);
+//}
 
 print $result;
-
 print $conn;
+?>
